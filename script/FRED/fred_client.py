63fb4416f3449bddc758b0d02ce4214e77b22ca7
MaddaGh <92437363+MaddaGh@users.noreply.github.com>
2023-06-22 12:32:32 +0200

    boh ho fatto un botto de roba nemmeno mi ricordo

---
+++ script/FRED/fred_client.py
@@ -0,0 +1,63 @@
+from fredclient import FREDClient, FREDParameters, FREDDefaults
+from rdflib import Graph, Namespace
+
+# fredclient
+key = "227e3c3c-d1e6-3fe6-8c26-a9552ec901db";
+fred_uri = "http://wit.istc.cnr.it/stlab-tools/fred"
+
+def parse_paragraph(sentence):
+
+    fredclient = FREDClient(fred_uri, key=key)
+    fredparams = FREDParameters(semantic_subgraph=False)
+    g = fredclient.execute_request(sentence, fredparams)
+
+    # changing namespaces for readabilty
+    
+    role = Namespace("http://www.ontologydesignpatterns.org/ont/vn/abox/role/")
+    sem = Namespace("http://ontologydesignpatterns.org/cp/owl/semiotics.owl#") #create a Namespace with the Namespace constructor method
+    schema = Namespace("http://www.w3.org/2006/03/wn/wn30/schema/")
+    earmark = Namespace("http://www.essepuntato.it/2008/12/earmark#")
+    quant = Namespace("http://www.ontologydesignpatterns.org/ont/fred/quantifiers.owl#")
+    dul = Namespace("http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#")
+    pos = Namespace("http://www.ontologydesignpatterns.org/ont/fred/pos.owl#")
+    fred = Namespace("http://www.ontologydesignpatterns.org/ont/fred/")
+    boxer  = Namespace("http://www.ontologydesignpatterns.org/ont/boxer/boxer.owl#")
+    boxing = Namespace("http://www.ontologydesignpatterns.org/ont/boxer/boxing.owl#")
+
+
+    g.bind("role", role)
+    g.bind("sem", sem)
+    g.bind("schema", schema)
+    g.bind("earmark", earmark)
+    g.bind("quant", quant)
+    g.bind("dul", dul)
+    g.bind("pos", pos)
+    g.bind("fred", fred)
+    g.bind("boxer", boxer)
+    g.bind("boxing", boxing)
+
+
+
+    
+    print(g.serialize(format="turtle"))
+
+
+
+
+
+'''def parse_text(text):
+    # split text in paragraphs
+    paragraphs = []
+    for paragraph in paragraphs:
+        parse_paragraph(paragraph)'''
+
+print(parse_paragraph('''Hannah Arendt and Virginia Woolf both understood the high stakes 
+of training the mind and imagination to go visiting, to venture off the 
+beaten path to meet unexpected, non-natal kin, and to strike up conversations, to pose and respond to interesting questions, to propose together something unanticipated, to take up the unasked-for obligations 
+of having met'''))
+# see what classes come out and align with tailored ontology
+# join each paragraph graph together ANZI no I have to make a graph that organizes the superstructure of the book and for every paragraph points to its graph
+
+
+
+
